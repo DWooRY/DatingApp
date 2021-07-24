40c526bef436d3fcfab864a918a67821feba4aca
Enis Can Yildiz <eniscanyldz_1282_@hotmail.com>
2021-07-24 17:47:06 +0300

    End Of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,8 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +55,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
