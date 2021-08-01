192cb695f75697ccbad5d359c54712947bc25d06
Enis Can Yildiz <eniscanyldz_1282_@hotmail.com>
2021-08-02 00:44:44 +0300

    End of section 14

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -1,16 +1,7 @@
 namespace API.Helpers
 {
-    public class UserParams
-    {
-        private const int MaxPageSize = 50;
-        public int PageNumber { get; set; }
-        private int _pageSize = 10;
-
-        public int PageSize{
-            get => _pageSize;
-            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
-        }
-
+    public class UserParams: PaginationParams
+    { 
         public string CurrentUsername { get; set; }
         public string Gender { get; set; }
         public int MinAge { get; set; } = 18;
---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+using API.Entities;
+using System.Collections.Generic;
+using API.DTOs;
+using API.Helpers;
+
+namespace API.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
+        Task<AppUser> GetUserWithLikes(int userId);
+        Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams);
+        
+    }
+}
