d6008fc60669b46178a337f4febb950d73805ca0
Enis Can Yildiz <eniscanyldz_1282_@hotmail.com>
2021-08-01 03:34:22 +0300

    End of section 13

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -45,8 +45,8 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
-
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
@@ -73,7 +73,8 @@ namespace API.Controllers
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,7 +1,9 @@
+using API.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [ApiController]
     [Route("api/[controller]")]
 
