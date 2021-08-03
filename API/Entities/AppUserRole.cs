fd10110ee698468613d44f968564db8debb84b83
Enis Can Yildiz <eniscanyldz_1282_@hotmail.com>
2021-08-03 21:43:52 +0300

    End of section 16

---
+++ API/Entities/AppUserRole.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Entities
+{
+    public class AppUserRole: IdentityUserRole<int>
+    {
+        public AppUser User { get; set; }
+        public AppRole Role { get; set; }
+    
+    
+    
+    }
+}
---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,47 @@
+using System.Text;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddIdentityCore<AppUser>(opt => 
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            }).AddRoles<AppRole>()
+                .AddRoleManager<RoleManager<AppRole>>()
+                .AddSignInManager<SignInManager<AppUser>>()
+                .AddRoleValidator<RoleValidator<AppRole>>()
+                .AddEntityFrameworkStores<DataContext>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => 
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
+            services.AddAuthorization(opt => 
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+            });
+
+            return services;
+        }
+    }
+}
