9d25463c7485c9dde695f2f0422429394cd9f209
Enis Can Yildiz <eniscanyldz_1282_@hotmail.com>
2021-08-06 15:00:29 +0300

    Connection string fix

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -53,7 +53,7 @@ namespace API.Extentions
                         var pgHost = pgHostPort.Split(":")[0];
                         var pgPort = pgHostPort.Split(":")[1];
 
-                        connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;TrustServerCertificate=True";
+                        connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}; SSL Mode=Require; Trust Server Certificate=true";
                     }
 
                     // Whether the connection string came from the local development configuration file
